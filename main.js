7936a9a7c520023ea2852bd2113a63d7b5544ce5
SAKTHI S M <smsakthi812@gmail.com>
2025-07-26 11:38:31 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+// const age: number = 25;
+// console.log(typeof age);
+// function testVar() {
+//   var globalVar = "I am a function-scoped variable";
+//   console.log(globalVar);
+// }
+// testVar();
+// const array1: number[] = [1, 2, 3];
+// console.log(typeof array1);
+function variable(var1) {
+    return var1;
+}
+console.log("1", variable("hello"));
---
+++ main.ts
@@ -0,0 +1,18 @@
+// const age: number = 25;
+// console.log(typeof age);
+
+// function testVar() {
+//   var globalVar = "I am a function-scoped variable";
+//   console.log(globalVar);
+// }
+// testVar();
+
+//Object type - array, enum,class,etc,
+// const array1: number[] = [1, 2, 3];
+// console.log(typeof array1);
+
+//Type definition
+// function variable(var1: string): string {
+//   return var1;
+// }
+// console.log("1", variable("hello"));
